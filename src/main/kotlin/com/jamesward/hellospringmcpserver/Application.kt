cf24006e3f19ceff35e4e09dff9aca1d6a53214a
James Ward <james@jamesward.com>
2025-05-09 11:32:11 -0600

    bump and expose num connections as a tool

--- src/main/kotlin/com/jamesward/hellospringmcpserver/Application.kt
+++ src/main/kotlin/com/jamesward/hellospringmcpserver/Application.kt
@@ -1,7 +1,8 @@
 package com.jamesward.hellospringmcpserver
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Statistic
 import org.springframework.ai.tool.annotation.Tool
-import org.springframework.ai.tool.annotation.ToolParam
 import org.springframework.ai.tool.method.MethodToolCallbackProvider
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -11,16 +12,22 @@ import org.springframework.stereotype.Service
 @SpringBootApplication
 class Application {
     @Bean
-    fun weatherTools(weatherService: WeatherService): MethodToolCallbackProvider =
-        MethodToolCallbackProvider.builder().toolObjects(weatherService).build()
+    fun tools(myTools: MyTools): MethodToolCallbackProvider =
+        MethodToolCallbackProvider.builder().toolObjects(myTools).build()
 }
 
 @Service
-class WeatherService {
+class MyTools(val meterRegistry: MeterRegistry) {
 
-    @Tool(description = "says hello to someone")
-    fun sayHello(@ToolParam(description = "name of person") name: String): String =
-        "hello, $name"
+    @Tool(description = "get the number of open connections to this server")
+    fun numConnections(): Double =
+        meterRegistry
+            .find("http.server.requests.active")
+//            .tag("uri", "/sse") // todo: there seems to be 5 active connections with the default usage. But this should be 1 and it'd be nice to filter to the /sse uri but that doesn't seem to work
+            .meter()
+            ?.measure()
+            ?.firstOrNull { it.statistic == Statistic.ACTIVE_TASKS }
+            ?.value ?: throw Exception("could not get number of connections")
 
 }
 
